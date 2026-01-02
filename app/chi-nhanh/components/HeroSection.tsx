16fc22e6220346f6740af088e890cf68e9736436
tuan45734 <tuanhvph45734@fpt.edu.vn>
2026-01-02 16:25:45 +0700

    commit2-1-3

--- app/chi-nhanh/components/HeroSection.tsx
+++ app/chi-nhanh/components/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
      <div className="w-full h-auto">
           <img 
-            src="https://res.cloudinary.com/dvg7ourbo/image/upload/v1766042090/pnpp1_tps2pb.png" 
+            src="https://res.cloudinary.com/dvg7ourbo/image/upload/v1767345892/bannerNPP_loucvv.jpg" 
             alt="Hero Background"
             className="w-full h-auto object-contain"
           />
--- app/gioi-thieu/components/HeroSection.tsx
+++ app/gioi-thieu/components/HeroSection.tsx
@@ -2,7 +2,7 @@ export default function HeroSection() {
   return (
      <div className="w-full h-auto">
           <img 
-            src="https://res.cloudinary.com/dvg7ourbo/image/upload/v1766042089/pngt1_xym9we.png" 
+            src="https://res.cloudinary.com/dvg7ourbo/image/upload/v1767345828/bannerGT_hxgugf.jpg" 
             alt="Hero Background"
             className="w-full h-auto object-contain"
           />
